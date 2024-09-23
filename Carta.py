ac6420f211a7e09f5a6097b86de4b0855b6e15d3
Amanda-Jr <amandadasilvapereira2004@gmail.com>
2024-09-23 18:19:23 -0300

    criacao carta e tabuleiro

---
+++ Carta.py
@@ -0,0 +1,15 @@
+class Carta:
+    
+    #gerar números aleatórios de 1 a 10
+    def __init__(self, dir, esq, cima, baixo):
+        self.dir = dir
+        self.esq = esq
+        self.cima = cima
+        self.baixo = baixo
+        
+    
+        
+    
+    
+    
+    
