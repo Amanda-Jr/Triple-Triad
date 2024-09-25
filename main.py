e8fb50a63e7a89b86695af520bb6b7420df90685
ViGF <vitoriogdef@gmail.com>
2024-09-25 08:30:42 -0300

    separa classe baralho da classe carta

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import Carta
+from Baralho import Baralho
 
-baralho = Carta.Baralho.distribuirCartas()
+baralho = Baralho.distribuirCartas()
 
 deck1 = baralho[0]
 deck2 = baralho[1]
-
+for c in deck1:
+    print(c)
